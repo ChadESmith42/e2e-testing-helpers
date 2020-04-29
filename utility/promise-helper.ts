9ec4bea278cde91ee2b285abd3de8a93370355c6
Chad Smith <Chad.Smith@BlueKC.com>
2020-04-29 10:47:08 -0500

    add metadata

--- utility/promise-helper.ts
+++ utility/promise-helper.ts
@@ -2,6 +2,8 @@ import { Logging } from './logging';
 
 /**
  * Returns a Promise<boolean> of true.
+ *
+ * @usage Use to 'resolve' promises that return void. Allows subsequent steps to break the chain if necessary.
  */
 export class PromiseHelper {
   public CompletePromise(): PromiseLike<any> {
@@ -13,9 +15,11 @@ export class PromiseHelper {
   }
 
   /**
-   * Returns a Promise<boolean> of false.
+   * Returns a Promise<boolean> of false and creates a log of the error.
    * @param identifier Text description of code block that failed (component, method, etc).
    * @param data OPTIONAL Array of data to be logged. Type is `any`. If not supplied, an empty array will be passed into the logging.
+   *
+   * @usage Use in `catch()` or to handle a 'failure' of a previous step.
    */
   public HandleError(identifier: string, data?: any[]): PromiseLike<any> {
     return new Promise((resolve, reject) => {

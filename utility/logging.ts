bb998659cf83cce09c11392b482ed08ce9a81099
Chad Smith <Chad.Smith@BlueKC.com>
2020-05-08 11:49:44 -0500

    logging async, added info

--- utility/logging.ts
+++ utility/logging.ts
@@ -6,21 +6,39 @@ export class Logging {
    * @param identifier String value to identify the code block (function, method, class, etc)
    * @param data OPTIONAL: Allows developer to pass additional information to be logged, such as variables, errors, additional comments for debugging.
    */
-  public LogError(identifier: string, data?: any[]) {
-    browser.executeScript(() => {
-      console.log(`Error in ${identifier}.`);
-      if (data.length > 0) {
-        for (const d in data) {
-          console.log(`Additional information: `, d);
+  public async LogError(identifier: string, data?: any[]) {
+    try {
+      return await browser.executeScript(() => {
+        console.error(`Error in ${identifier}.`);
+        if (data.length > 0) {
+          for (const d in data) {
+            console.log(`Additional information: `, d);
+          }
         }
-      }
-    }).then(resolve => {
-      return new Promise<boolean>((resolve, reject) => {
-        resolve(true);
-        reject(false);
       });
-    }).catch(() => {
-      alert(`Logging Error. Check ${identifier}`);
-    });
+    } catch (error) {
+      await browser.executeScript(() => {
+        console.error('Failed to log error', [ identifier, data ]);
+      });
+      throw error;
+    }
+  }
+
+  public async LogInfo(identifier: string, data?: any[]) {
+    try {
+      return await browser.executeScript(() => {
+        console.info(`Info for ${identifier}.`);
+        if (data.length > 0) {
+          for (const d in data) {
+            console.info(`Additional information: `, d);
+          }
+        }
+      });
+    } catch (error) {
+      await browser.executeScript(() => {
+        console.error('Failed to log info', [ identifier, data ]);
+      });
+      throw error;
+    }
   }
 }
